0e92f553afaa2707e796aa4cdf904941871e5c89
slow <73202350+slow@users.noreply.github.com>
2020-10-22 01:39:22 +0100

    release

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Plugin } = require('powercord/entities');
+const { getModule, spotify } = require('powercord/webpack');
+const Modules = getModule(['initialize', 'wasAutoPaused'], false);
+
+module.exports = class FuckAutoPause extends Plugin {
+   startPlugin() {
+      Modules._wasAutoPaused = Modules.__proto__.wasAutoPaused;
+      Modules.__proto__.wasAutoPaused = () => false;
+      spotify._pause = spotify.pause;
+      spotify.pause = () => void 0;
+   }
+
+   pluginWillUnload() {
+      Modules.__proto__.wasAutoPaused = Modules._wasAutoPaused;
+      spotify.pause = spotify._pause;
+   }
+}
